95a938a504241afd085847480cffac76cf4f18f4
Heather Hartley <hlhartley@gmail.com>
2018-10-30 23:20:55 -0600

    Add media queries

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ hamburger.addEventListener('click', toggleHamburger);
 function toggleHamburger() {
   hamburger.classList.toggle('toggled-hamburger');
 }
+
+var modal = document.querySelector('.modal');
+var modalBtn = document.querySelector('.caret-wrapper');
+
+function toggleModal() {
+  modal.classList.toggle('toggled-modal')
+}
